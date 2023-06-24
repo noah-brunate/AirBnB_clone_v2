42546fb45886a15e93391394acea123dacbc4c20
Tinashe <nattymachona2000@gmail.com>
2023-06-24 15:33:46 -0700

    DBStorage - Amenity and Place

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 
-class Amenity(BaseModel):
-    name = ""
+stored = os.environ.get('HBNB_TYPE_STORAGE')
+if stored == 'db':
+    from models.place import place_amenity
+
+class Amenity(BaseModel, Base):
+    """Amenity Class """
+    stored = os.environ.get('HBNB_TYPE_STORAGE')
+    if stored == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity)
+    else:
+        name = ""
--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 
-class State(BaseModel):
+store_type = os.getenv('HBNB_TYPE_STORAGE')
+
+
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if store_type == 'db':
+        cities = relationship('City', backref='state', cascade="all, delete")
+    else:
+        @property
+        def cities(self):
+            """Getter attribute"""
+            from models import storage
+            list_cities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
