5c55cc4e97c997bb5f457afca643d2ee39819ad8
noah-brunate <noahbrunate9@gmail.com>
2023-06-24 23:06:46 -0700

    uqdates

--- models/state.py
+++ models/state.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", back_populates="states", cascade="all, delete")
+
+    @property
+    def cities(self):
+        """returns the list of City instances """
+
+        return self.cities
