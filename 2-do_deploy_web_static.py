972cd6d152dc9ec269ef2ebd04021a800a264c19
noah-brunate <noahbrunate9@gmail.com>
2023-07-20 04:57:52 -0700

    fabric

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 env.hosts = ['100.25.142.198', '54.237.15.47']
 env.user = 'ubuntu'
-env.key_filename = 'private'
+env.key_filename = '../private'
 
 
 def do_deploy(archive_path):
@@ -20,15 +20,15 @@ def do_deploy(archive_path):
 
         put(archive_path, '/tmp/')
 
-        timestamp = archive_path[-18, -4]
-        run("sudo mkdir -p /data/web_static/releases/web_static_{}".format(
-            timestamp))
+        timestamp = archive_path[-18:-4]
+        run('sudo mkdir -p /data/web_static/\
+releases/web_static_{}/'.format(timestamp))
 
         # uncompress the archive
         run("sudo tar -xzf /tmp/web_static_{}.tgz -C /data/web_static/\
 releases/web_static_{}/".format(timestamp, timestamp))
-        run("sudo mv /data/web_static/releases/web_static_{}/web_static/*\
-/data/web_static/releases/web_static_{}/".format(timestamp, timestamp))
+        run("sudo mv /data/web_static/releases/web_static_{}/web_static/* /data\
+/web_static/releases/web_static_{}/".format(timestamp, timestamp))
 
         run("sudo rm -rf /data/web_static/releases/web_static_{}/web_static")
         run("sudo rm /tmp/web_static_{}.tgz".format(timestamp))
