fe56577deeb86fdb7f3432bdfbf213672336a5ee
noah-brunate <noahbrunate9@gmail.com>
2023-07-21 02:53:22 -0700

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,12 +3,11 @@
 
 import os
 from fabric.api import *
-from datetime import datetime
 
 
 env.hosts = ['100.25.142.198', '54.237.15.47']
 env.user = 'ubuntu'
-env.key_filename = '../private'
+env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
@@ -27,11 +26,14 @@ releases/web_static_{}/'.format(timestamp))
         # uncompress the archive
         run("sudo tar -xzf /tmp/web_static_{}.tgz -C /data/web_static/\
 releases/web_static_{}/".format(timestamp, timestamp))
-        run("sudo mv /data/web_static/releases/web_static_{}/web_static/* /data\
-/web_static/releases/web_static_{}/".format(timestamp, timestamp))
 
-        run("sudo rm -rf /data/web_static/releases/web_static_{}/web_static")
         run("sudo rm /tmp/web_static_{}.tgz".format(timestamp))
+
+        run("sudo mv /data/web_static/releases/web_static_{}/web_static\
+/* /data/web_static/releases/web_static_{}/".format(timestamp, timestamp))
+
+        run("sudo rm -rf /data/web_static/releases/web_static_{}/\
+web_static".format(timestamp))
 
         run('sudo rm -rf /data/web_static/current')
         run('sudo ln -s /data/web_static/releases/\
