85f8a08364504286783ffed33b273aa1e6a2fdb5
noah-brunate <noahbrunate9@gmail.com>
2023-07-22 13:22:50 -0700

    flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """Script starts a Flask web application:
     -listening on 0.0.0.0, port 5000
-    -Routes:
-        /: display “Hello HBNB!”
 """
 
 from flask import Flask
 
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
 def hello():
     """Returns a given string"""
-    return (“Hello HBNB!”)
+    return “Hello HBNB!”
 
 
 if __name__ == '__main__':
